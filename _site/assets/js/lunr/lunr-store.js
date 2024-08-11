416420eb120b9a4e1394cbd2d36dee9476364878
hengyinayo <wuheng@yonsei.ac.kr>
2024-08-11 19:39:12 +0900

    feat: 댓글 기능 추가

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "첫 포스팅 (테스트)",
+        "excerpt":"블로그 테스트   테스트 테스트 테스트   실시간 반영 테스트     ","categories": [],
+        "tags": [],
+        "url": "/first/",
+        "teaser": null
+      },{
+        "title": "ipybb 테스팅",
+        "excerpt":"print(\"hello\")   hello     ","categories": [],
+        "tags": [],
+        "url": "/testIpynb/",
+        "teaser": null
+      }]
