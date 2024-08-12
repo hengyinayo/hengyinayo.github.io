1bebed5ddf6dfd783f5a1a013d1a27682cdb1aa8
hengyinayo <wuheng@yonsei.ac.kr>
2024-08-12 18:39:56 +0900

    feat: 08/12 Ch15 가상면접사례로배우는대규모시스템설계기초

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,9 +6,9 @@ var store = [{
         "teaser": null
       },{
         "title": "ipybb 테스팅",
-        "excerpt":"print(\"hello\")   hello     ","categories": [],
-        "tags": [],
-        "url": "/testIpynb/",
+        "excerpt":"print(\"hello\")   hello     ","categories": ["testing"],
+        "tags": ["test"],
+        "url": "/testing/test2/",
         "teaser": null
       },{
         "title": "Github Blog (with Minimal Mistakes)",
@@ -21,5 +21,11 @@ var store = [{
         "excerpt":"BFS 맹목적 탐색방법의 하나로 시작 정점을 방문한 후 시작 정점에 인접한 모든 정점들을 우선 방문하는 방법. 더 이상 방문하지 않은 정점이 없을 때까지 방문하지 않은 모든 node들에 대해서 BFS를 적용. 특징: queue를 사용 그래야 레벨 순서대로 접근 가능 문제 해결: 시작하는 칸을 방문했다는 표시를 하고 큐에 넣어줌 큐의 front에 있는...","categories": ["Algorithm"],
         "tags": ["C++"],
         "url": "/algorithm/BFS/",
+        "teaser": null
+      },{
+        "title": "Ch15 구글 드라이브 설계",
+        "excerpt":"블로그    ","categories": ["가상면접설계로배우는대규모시스템설계기초"],
+        "tags": ["가상면접설계로배우는대규모시스템설계기초"],
+        "url": "/%EA%B0%80%EC%83%81%EB%A9%B4%EC%A0%91%EC%84%A4%EA%B3%84%EB%A1%9C%EB%B0%B0%EC%9A%B0%EB%8A%94%EB%8C%80%EA%B7%9C%EB%AA%A8%EC%8B%9C%EC%8A%A4%ED%85%9C%EC%84%A4%EA%B3%84%EA%B8%B0%EC%B4%88/Ch15-%EA%B5%AC%EA%B8%80-%EB%93%9C%EB%9D%BC%EC%9D%B4%EB%B8%8C-%EC%84%A4%EA%B3%84/",
         "teaser": null
       }]
